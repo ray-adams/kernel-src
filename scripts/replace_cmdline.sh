f0eefa5c553c5eb839ae9ad47d13d517cd2b77ee
ray <ray@salaciouswind.com>
2024-06-26 16:41:47 +0100

    Removing cd and specifying directory via a variable

--- scripts/replace_cmdline.sh
+++ scripts/replace_cmdline.sh
@@ -7,21 +7,19 @@
 # Copyright 2024 Ray Adams
 # SPDX-Licence-Identifier: BSD-3-Clause
 
-# Version: 2.0.1
+# Version: 2.1.1
 
 working_dir="$(git rev-parse --show-toplevel)"
-
-cd ${working_dir}
 
 angelica_cmd_line="rootfstype=bcachefs root=UUID=<root_uuid> nosmt=force intel_iommu=on"
 eleanore_cmd_line="root=PARTUUID=<root_partuuid> nosmt=force"
 kotori_cmd_line="rootfs_type=bcachefs root=UUID=<root_uuid> nosmt=force intel_iommu=on"
 
-find "./configs/angelica/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${angelica_cmd_line}\"/" {} + \
+find "${working_dir}/configs/angelica/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${angelica_cmd_line}\"/" {} + \
     || { echo "Error replacing angelica command line parameters."; exit 1; }
-find "./configs/eleanore/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${eleanore_cmd_line}\"/" {} + \
+find "${working_dir}/configs/eleanore/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${eleanore_cmd_line}\"/" {} + \
     || { echo "Error replacing eleanore command line parameters."; exit 1; }
-find "./configs/kotori/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${kotori_cmd_line}\"/" {} + \
+find "${working_dir}/configs/kotori/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${kotori_cmd_line}\"/" {} + \
     || { echo "Error replacing kotori command line parameters."; exit 1; }
 
 echo "Finished replacing command line parameters."
