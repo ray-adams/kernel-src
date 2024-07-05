5d5f826d8df36714d72008eadf00c806b333c422
ray <ray@salaciouswind.com>
2024-07-05 22:13:45 +0100

    Adding color to messages

--- scripts/create_backup_kernel.sh
+++ scripts/create_backup_kernel.sh
@@ -7,17 +7,22 @@
 # Copyright 2024 Ray Adams
 # SPDX-Licence-Identifier: BSD-3-Clause
 
-# Version: 1.1.1
+# Version: 1.2.1
+
+# Colors
+green='\033[0;32m'
+red='\033[0;31m'
+nc='\033[0m'
 
 # Check if the script was executed with root privilages.
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this shell script as root."
+    echo "${green}Please run this shell script as root.${nc}"
     exit 1
 fi
 
 # Copy current kernel to backup location.
 mount /boot
-cp "/boot/efi/boot/bootx64.efi" "/boot/efi/boot/backup.efi" || { echo "Error copying current kernel to backup.efi"; exit 1; }
+cp "/boot/efi/boot/bootx64.efi" "/boot/efi/boot/backup.efi" || { echo "${red}Error copying current kernel to backup.efi.${nc}"; exit 1; }
 umount /boot
 
-echo "Copied $(uname -r) to /boot/efi/boot/backup.efi."
+echo "${green}Copied $(uname -r) to /boot/efi/boot/backup.efi.${nc}"
