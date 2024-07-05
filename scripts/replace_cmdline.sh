f9eda8a65afb5a7d82edcf1c1c0b26fc86c5943a
ray <ray@salaciouswind.com>
2024-07-05 04:14:02 +0100

    Adding new color as well as new params

--- scripts/replace_cmdline.sh
+++ scripts/replace_cmdline.sh
@@ -7,19 +7,26 @@
 # Copyright 2024 Ray Adams
 # SPDX-Licence-Identifier: BSD-3-Clause
 
-# Version: 2.1.1
+# Version: 2.2.1
 
+# Obtain the path for <git_root>
 working_dir="$(git rev-parse --show-toplevel)"
 
-angelica_cmd_line="rootfstype=bcachefs root=UUID=<root_uuid> nosmt=force intel_iommu=on"
-eleanore_cmd_line="root=PARTUUID=<root_partuuid> nosmt=force"
-kotori_cmd_line="rootfs_type=bcachefs root=UUID=<root_uuid> nosmt=force intel_iommu=on"
+# Colors
+green='\033[0;32m'
+red='\033[0;31m'
+nc='\033[0m'
+
+# CMDLINE parameters
+angelica_cmd_line="rootfstype=bcachefs root=UUID=<root_uuid> nosmt=force intel_iommu=on quiet"
+eleanore_cmd_line="root=PARTUUID=<root_partuuid> nvidia_drm.fbdev=1 nosmt=force quiet"
+kotori_cmd_line="rootfs_type=bcachefs root=UUID=<root_uuid> nosmt=force intel_iommu=on quiet"
 
 find "${working_dir}/configs/angelica/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${angelica_cmd_line}\"/" {} + \
-    || { echo "Error replacing angelica command line parameters."; exit 1; }
-find "${working_dir}/configs/eleanore/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${eleanore_cmd_line}\"/" {} + \
-    || { echo "Error replacing eleanore command line parameters."; exit 1; }
+    || { echo "${red}Error replacing angelica command line parameters.${nc}"; exit 1; }
+find "${working_dir}/configs/eleanore-compile/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${eleanore_cmd_line}\"/" {} + \
+    || { echo "${red}Error replacing eleanore command line parameters.${nc}"; exit 1; }
 find "${working_dir}/configs/kotori/" -type f -exec sed -i "s/^CONFIG_CMDLINE=.*/CONFIG_CMDLINE=\"${kotori_cmd_line}\"/" {} + \
-    || { echo "Error replacing kotori command line parameters."; exit 1; }
+    || { echo "${red}Error replacing kotori command line parameters.${nc}"; exit 1; }
 
-echo "Finished replacing command line parameters."
+echo "${green}Finished replacing command line parameters.${nc}"
