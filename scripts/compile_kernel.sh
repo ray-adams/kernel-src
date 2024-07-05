c20b0ad87c968ac54a86c9c9bfac90a15e0aac7c
ray <ray@salaciouswind.com>
2024-07-05 20:26:14 +0100

    Compilations now occur in tmpfs

--- scripts/compile_kernel.sh
+++ scripts/compile_kernel.sh
@@ -7,7 +7,7 @@
 # Copyright 2024 Ray Adams
 # SPDX-Licence-Identifier: BSD-3-Clause
 
-# Version: 2.4.1
+# Version: 3.0.0
 
 # Default source path
 src_path="/usr/local/src/"
@@ -17,19 +17,21 @@ green='\033[0;32m'
 red='\033[0;31m'
 nc='\033[0m'
 
-# Check if the script was executed with root privilages.
+# Check if the script was executed with root privilages
 if [ "$(id -u)" -ne 0 ]; then
     echo "${red}Please run this shell script as root.${nc}"
     exit 1
 fi
 
-# Choose available kernel version for system.
+# Choose available kernel version for system
 select_version() {
+    # Check if the source directory exists for chosen system
     if [ ! -e "${src_path}/${system}/" ]; then
         echo "${red}The directory ${src_path}/${system} does not exist.${nc}"
         exit 1
     fi
 
+    # Ask user to choose a kernel version
     echo "${green}Available linux kernels:${nc}"
     available_versions="$(ls "${src_path}/${system}/linux/")"
     echo "${green}${available_versions}${nc}"
@@ -41,22 +43,42 @@ select_version() {
             break
         elif [ -d "${src_path}/${system}/linux/linux-${version}/" ]; then
             linux_src_path="${src_path}/${system}/linux/linux-${version}/"
+            version="linux-${version}"
             break
         else
             echo "${red}Please choose a valid version.${nc}"
         fi
     done
 
+    # Local version for selected linux version
     local_version="$(echo ${version} | sed 's/linux-//')-$(grep "^CONFIG_LOCALVERSION" "${linux_src_path}/.config" | sed 's/^CONFIG_LOCALVERSION="-//' | tr -d '"')"
-
-    cd ${linux_src_path}
 }
 
-# Compile the kernel without an initramfs.
+# Compile the kernel without an initramfs
 compile_kernel() {
+    compile_dir="/var/tmp/linux/${system}/"
+
+    # Check if tmpfs directory exists
+    if [ ! -d "${compile_dir}" ]; then
+        echo "${red}tmpfs directory ${compile_dir} not found.${nc}"
+        exit 1
+    fi
+
+    # Mount tmpfs directory
+    mount "${compile_dir}" || { echo "${red}Error mounting tmpfs directory ${compile_dir}.${nc}"; exit 1; }
+
+    # Copy source directory to tmpfs
+    cp -r "${linux_src_path}" "${compile_dir}" || { echo "${red}Error copying ${local_version} to tmpfs directory.${nc}"; exit 1; }
+
+    # Compile the kernel
+    cd "${compile_dir}/${version}/"
     make -j6 || { echo "${red}Error compiling kernel ${local_version}.${nc}"; exit 1; }
 
-    cp "${linux_src_path}/arch/x86/boot/bzImage" "${src_path}/${system}/vmlinuz/vmlinuz-${local_version}.efi" || { echo "${red}Error copying vmlinuz-${local_version}.efi to source directory.${nc}"; exit 1; }
+    cp "${compile_dir}/${version}/arch/x86/boot/bzImage" "${src_path}/${system}/vmlinuz/vmlinuz-${local_version}.efi" || { echo "${red}Error copying vmlinuz-${local_version}.efi to source directory.${nc}"; exit 1; }
+
+    # Remove kernel directory from tmpfs
+    rm -r "${compile_dir}/${version}/" || { echo "${red}Error removing ${local_version} from tmpfs.${nc}"; exit 1; }
+    umount "${compile_dir}" || { echo "${red}Error unmounting tmpfs.${nc}"; exit 1; }
 
     echo "${green}Finished creating ${local_version} kernel image.${nc}"
 }
@@ -72,16 +94,35 @@ copy_to_boot() {
 
 # Compile and sign the unified kernel image.
 compile_uki() {
+    compile_dir="/var/tmp/linux/${system}/"
     initramfs_path="${src_path}/${system}/initramfs/initramfs-${system}.cpio"
 
+    # Check if tmpfs directory exists
+    if [ ! -d "${compile_dir}" ]; then
+        echo "${red}tmpfs directory ${compile_dir} not found.${nc}"
+        exit 1
+    fi
+
+    # Mount tmpfs directory
+    mount "${compile_dir}" || { echo "${red}Error mounting tmpfs directory ${compile_dir}.${nc}"; exit 1; }
+
+    # Copy source directory to tmpfs
+    cp -r "${linux_src_path}" "${compile_dir}" || { echo "${red}Error copying ${local_version} to tmpfs directory.${nc}"; exit 1; }
+
+    # Compile the kernel
+    cd "${compile_dir}/${version}/"
     LD_PRELOAD="" make -j6 || { echo "${red}Error compiling kernel ${local_version}.${nc}"; exit 1; }
     make modules_install || { echo "${red}Error compiling modules to /lib/modules/${local_version}/.${nc}"; exit 1; }
 
     dracut -f --kver=${local_version} ${initramfs_path} || { echo "${red}Error creating dracut initramfs image for ${local_version}.${nc}"; exit 1; }
 
     LD_PRELOAD="" make -j6 || { echo "${red}Error compiling kernel ${local_version} with the new initramfs image.${nc}"; exit 1; }
-    sbsign --key "/etc/keys/efikeys/db.key" --cert "/etc/keys/efikeys/db.crt" --output "${src_path}/${system}/uki/vmlinuz-${local_version}.efi" "${linux_src_path}/arch/x86/boot/bzImage" \
+    sbsign --key "/etc/keys/efikeys/db.key" --cert "/etc/keys/efikeys/db.crt" --output "${src_path}/${system}/uki/vmlinuz-${local_version}.efi" "${compile_dir}/${version}/arch/x86/boot/bzImage" \
         || { echo "${red}Error signing unified kernel image vmlinuz-${local_version}.efi.${nc}"; exit 1; }
+
+    # Remove kernel directory from tmpfs
+    rm -r "${compile_dir}/${version}/" || { echo "${red}Error removing ${local_version} from tmpfs.${nc}"; exit 1; }
+    umount "${compile_dir}" || { echo "${red}Error unmounting tmpfs.${nc}"; exit 1; }
 
     echo "${green}Finished creating ${local_version} UKI.${nc}"
 }
