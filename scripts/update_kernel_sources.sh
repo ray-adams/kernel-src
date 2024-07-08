f1e0ec784fc6bc98e8fa80a3f65da4d9d36bdbdf
ray <ray@salaciouswind.com>
2024-07-08 19:56:03 +0100

    Version bump

--- scripts/update_kernel_sources.sh
+++ scripts/update_kernel_sources.sh
@@ -7,10 +7,10 @@
 # Copyright 2024 Ray Adams
 # SPDX-Licence-Identifier: BSD-3-Clause
 
-# Version: 2.0.0
+# Version: 3.0.0
 
+# New kernel version installed by portage
 new_kernel_ver="$(ls "/usr/src/" --hide="linux" | grep "linux")"
-systems="angelica kotori"
 
 # Colors
 green='\033[0;32m'
@@ -23,13 +23,13 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Remove old kernel sources in /usr/src/
-EMERGE_DEFAULT_OPTS="" emerge --prune --nodeps sys-kernel/vanilla-sources
-
-# For loop runs commands for all systems listed in variable.
-for system in ${systems}
-do
+copy_kernel_source() {
     src_path="/usr/local/src/${system}/"
+    prev_kernel_ver="$(ls "${src_path}/linux/" | sort -V | tail -n 1)"
+
+    # Remove old kernel sources in /usr/src/
+    EMERGE_DEFAULT_OPTS="" emerge --prune --nodeps sys-kernel/vanilla-sources
+
     # Check if kernel source already exists. If so skip.
     if [ -d "${src_path}/linux/${new_kernel_ver}" ]; then
         echo "${green}${new_kernel_ver} already exists for ${system}. Skipping...${nc}\n"
@@ -37,42 +37,43 @@ do
     fi
 
     # Copy the new kernel version to the source directory.
-    echo "${green}Copying ${new_kernel_ver} to ${src_path}/linux/...${nc}"
     cp -r "/usr/src/${new_kernel_ver}" "${src_path}/linux/" || { echo "${red}Error copying ${new_kernel_ver} to ${system}'s kernel source directory.${nc}"; exit 1; }
+
     echo "${green}Successfully copied ${new_kernel_ver} to ${system}'s kernel source directory.${nc}"
 
     # Copy the old configuration to the new source directory.
-    prev_kernel_ver="$(ls "${src_path}/linux/" | sort -V | tail -n 2 | head -n 1)"
-
     cp "${src_path}/linux/${prev_kernel_ver}/.config" "${src_path}/linux/${new_kernel_ver}/" || { echo "${red}Error copying previous linux configuration to ${system}/${new_kernel_ver}${nc}"; exit 1; }
-    echo "${green}Successfully copied old kernel configuration to ${system}'s kernel source directory.${nc}\n"
 
+    echo "${green}Successfully copied old kernel configuration to ${system}'s kernel source directory.${nc}\n"
+}
+
+make_oldconfig() {
     # Ask if user wants to do a make oldconfig.
     while true; do
         echo "${green}Do you want to make oldconfig? (y/n):${nc}"
         read make_oldconfig_ans
         case ${make_oldconfig_ans} in
             [Yy]* ) 
-                cd "${src_path}/linux/${new_kernel_ver}"
+                prev_conf_local_ver="$(grep "^CONFIG_LOCALVERSION" "${src_linux}/linux/${new_kernel_ver}/.config" | sed "s/^CONFIG_LOCALVERSION=\"-${system}-//" | tr -d '"')"
 
+                cd "${src_path}/linux/${new_kernel_ver}"
                 make oldconfig || { echo "${red}Error while doing make oldconfig for ${new_kernel_ver}${nc}"; exit 1; }
 
-                prev_conf_local_ver="$(grep "^CONFIG_LOCALVERSION" ".config" | sed "s/^CONFIG_LOCALVERSION=\"-${system}-//" | tr -d '"')"
-
+                # Allow the user to choose a new local version for the new kernel.
                 while true; do
                     echo "${green}The previous local version was ${prev_conf_local_ver} for ${prev_kernel_ver}. Please enter a new local version.${nc}"
                     read new_local_ver_ans
                     
                     if echo "${new_local_ver_ans}"  | grep -q "^[0-9]\+\.[0-9]\+\.[0-9]\+$"; then
                         sed -i "s/-${system}-${prev_conf_local_ver}/-${system}-${new_local_ver_ans}/" ".config" || { echo "${red}Error writing new local version.${nc}"; exit 1; }
-                        echo "${green}Successfully updated local version for ${system}/${new_local_ver_ans}.${nc}\n"
 
                         break
                     else
                         echo "${red}Please enter a valid version string.${nc}"
                     fi
                 done
-
+                
+                echo "${green}Successfully updated local version for ${system}/${new_local_ver_ans}.${nc}\n"
                 break
             ;;
     
@@ -86,9 +87,37 @@ do
             ;;
         esac
     done
-done
+}
 
-# Update symlink for kotori.
-rm /usr/src/linux || { echo "${red}Failed to remove old symlink to ${prev_kernel_ver}.${nc}"; exit 1; }
-ln -s "${src_path}/linux/${new_kernel_ver}/" "/usr/src/linux" || { echo "${red}Error creating symlink to new kotori linux source directory.${nc}"; exit 1; }
-echo "${green}Sucessfully created /usr/src/linux symlink to ${src_path}/linux/${new_kernel_ver}${nc}"
+update_symlink() {
+    # Update symlink for to new kernel source dir.
+    rm /usr/src/linux || { echo "${red}Failed to remove old symlink to ${prev_kernel_ver}.${nc}"; exit 1; }
+    ln -s "${src_path}/linux/${new_kernel_ver}/" "/usr/src/linux" || { echo "${red}Error creating symlink to ${src_path}/linux/${new_kernel_ver}.${nc}"; exit 1; }
+
+    echo "${green}Sucessfully created /usr/src/linux symlink to ${src_path}/linux/${new_kernel_ver}${nc}"
+}
+
+# Allow the user to select which system to compile a kernel for.
+case ${1} in
+    angelica)
+        system="angelica"
+        copy_kernel_source && make_oldconfig
+    ;;
+
+    eleanore)
+        system="eleanore"
+        copy_kernel_source && make_oldconfig && update_symlink
+        
+    ;;
+
+    kotori)
+        system="kotori"
+        copy_kernel_source && make_oldconfig && update_symlink
+    ;;
+
+    *)
+        echo "Unkown option: \"${1}\""
+        echo "Correct Usuage: ${0} [SYSTEM]"
+        echo "Available systems: angelica, eleanore, kotori"
+    ;;
+esac
